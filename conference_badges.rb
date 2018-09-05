71e2eb132a23866c889c8b05ef9f12b2f5c855d1
christine gomez <christine.gomez1226@gmail.com>
2018-09-05 01:07:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,3 +22,12 @@ def assign_rooms(attendees)
   end
   return room_assignments
 end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge 
+  end
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end
