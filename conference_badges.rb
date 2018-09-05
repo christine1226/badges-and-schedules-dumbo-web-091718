72d6edaae0595237baedec41c37aa7325db4e01e
christine gomez <christine.gomez1226@gmail.com>
2018-09-05 00:30:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  name = "Arel"
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  "Hello, my name is #{attendees}."
+end
